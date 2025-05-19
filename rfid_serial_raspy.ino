5ea9c10e30c66d1747a7fa8288db91ac89dd01bd
mustofaadi <mustofaadi663@gmail.com>
2025-05-19 15:26:49 +0700

    Add files via upload

---
+++ rfid_serial_raspy.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 5
+#define RST_PIN 22
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(115200); // UART to Raspy
+  SPI.begin();
+  mfrc522.PCD_Init();
+  delay(1000);
+}
+
+void loop() {
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    Serial.print("UID:");
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+    }
+    Serial.println();
+    delay(1000);
+  }
+}
